fd2604096a2e75b08597ddb5abbc71063696e3e4
Wojciech Szewczyk <wojciech.szewczyk@stazysta.comarch.pl>
2022-09-20 17:22:03 +0200

    estetic changes, changed .png icon to .xml moved colors to color resources

--- app/src/main/java/com/example/ble2/ui/adapter/subnet/AddSubnetDialog.kt
+++ app/src/main/java/com/example/ble2/ui/adapter/subnet/AddSubnetDialog.kt
@@ -9,18 +9,18 @@ import androidx.appcompat.app.AppCompatDialogFragment
 import com.example.ble2.databinding.LayoutDialogBinding
 
 class AddSubnetDialog(val viewModel: SubnetsViewModel) : AppCompatDialogFragment() {
-    private lateinit var bindig: LayoutDialogBinding
+    private lateinit var binding: LayoutDialogBinding
 
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
-        bindig = LayoutDialogBinding.inflate(LayoutInflater.from(context))
+        binding = LayoutDialogBinding.inflate(LayoutInflater.from(context))
         val builder = AlertDialog.Builder(requireActivity())
 
 
-        builder.setView(bindig.root).setTitle("Select Subnet Name")
+        builder.setView(binding.root).setTitle("Select Subnet Name")
             .setPositiveButton("ok", DialogInterface.OnClickListener() { dialog, which ->
-                viewModel.setName(bindig.subnetNameDialog.text.toString())
+                viewModel.setName(binding.subnetNameDialog.text.toString())
             })
         return builder.create()
     }
